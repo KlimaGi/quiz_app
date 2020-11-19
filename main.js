3a6b9934ac95095a61aee614519ea951263bf31e
Giedre Klimaite <giedre.klima@gmail.com>
2020-11-19 23:40:44 +0200

    quiz_app begining

---
+++ main.js
@@ -0,0 +1,33 @@
+var quizData = {
+  {
+    question: "Short cut to open new tab in Windows?",
+    a: "Ctrl + T",
+    b: "Ctrl + 0",
+    c: "Alt + Z",
+    correct: "a"
+  }, {
+question: "Short cut to create new doc in Windows?",
+    a: "Ctrl + T",
+    b: "Ctrl + N",
+    c: "Ctrl + C",
+    correct: "b"
+  }, {
+    question: "Short cut to stop running code?",
+    a: "Ctrl + T",
+    b: "Ctrl + C",
+    c: "Alt + T",
+    correct: "b"
+  }, {
+    question: "Short cut for toggle word wrap in vscode?",
+    a: "Alt + N",
+    b: "Ctrl + 0",
+    c: "Alt + Z",
+    correct: "c"
+  }, {
+  question: "Short cut to select multiple lines in vscode?",
+    a: "Ctrl + Alt + arrow",
+    b: "Ctrl + arrows",
+    c: "Alt + arrows",
+    correct: "a"
+  }
+};
